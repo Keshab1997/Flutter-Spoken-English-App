4e99074d95b4bf5ced3bca05e32fbd3cc0e5dcf3
Kilo <kilo@kilo.ai>
2026-06-29 16:27:16 +0530

    update

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,5 +1,6 @@
 import 'dart:typed_data';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
